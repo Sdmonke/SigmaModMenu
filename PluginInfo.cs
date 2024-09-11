c0ecf5b9e385c788f6a6de2378f0ba4f86431793
Sdmonke <141530939+Sdmonke@users.noreply.github.com>
2024-09-10 20:35:25 -0500

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.sdmonke.gorillatag.menutemplate";
+        public const string Name = "SDmonke's Sigma Mod Menu";
+        public const string Description = "Created by sdmonke";
+        public const string Version = "1.0.0";
+    }
+}
